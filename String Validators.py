9e485c306be886def8c87c46ed3305a7bf08aa8d
Sanskriti <sanskritisri27@gmail.com>
2023-11-15 14:41:08 +0530

    solved Text Wrap
    Simpler than what I imagined

---
+++ String Validators.py
@@ -0,0 +1,11 @@
+# QUESTION : https://www.hackerrank.com/challenges/string-validators/problem
+
+# SOLUTION:
+if __name__ == '__main__':
+    s = input()
+    
+    print (any([char.isalnum() for char in s]))
+    print (any([char.isalpha() for char in s]))
+    print (any([char.isdigit() for char in s]))
+    print (any([char.islower() for char in s]))
+    print (any([char.isupper() for char in s]))
